105bdaf75fe3a8dac56b0b80e933c1e93a1fd013
Josh Parkinson <parkinsonxc@gmail.com>
2017-10-05 15:21:36 -0500

    Okay, so this works and I don't understand why. When adding a new blog, it takes you to the individual post page (as it should) but the URL is still /newpost and there is now query param. Hmm..

--- main.py
+++ main.py
@@ -56,7 +56,9 @@ def add_blog():
         new_post = Blog(blog_title, blog_body)
         db.session.add(new_post)
         db.session.commit()
-        return redirect('/blog')
+        blog = Blog.query.get(new_post.id)
+        
+        return render_template("displaypost.html", blog=blog)
 
     else:
         return render_template('newpost.html',
